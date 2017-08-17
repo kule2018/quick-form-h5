5e0353396859c2d31a8cdb942d9a12ca9a31c831
ajie <yajieliu@tencent.com>
2017-08-17 22:55:31 +0800

    添加动画效果！

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,39 @@
 <template>
   <div id="app">
-    <transition :name="transitionName">
-      <router-view></router-view>
+    <transition
+    v-on:enter="enter"
+		v-on:leave="leave"
+		v-bind:css="false"
+    >
+      <router-view class="page"></router-view>
     </transition>
   </div>
 </template>
 
 
 <script>
-export default {
-  name: 'app',
+  import Animate from '@/utils/animate';
 
-  data () {
+  export default {
+    name: 'app',
+
+    data() {
       return {
-          transitionName: 'slide'
       }
-  },
+    },
 
-  watch: {
-    '$route' (to, from) {
-      const toDepth = to.path.split('/').length;
-      const fromDepth = from.path.split('/').length;
-      console.log(to.path,from.path,toDepth,fromDepth);
-
-      this.transitionName = toDepth < fromDepth ? 'slide-right' : 'slide-left'
-    }
-  },
-
-  beforeRouteUpdate(to,from,next){
-        let isBack = this.$router.isBack;
-        if( isBack ){
-            this.transitionName = 'slide-right'
-        }else{
-            this.transitionName = 'slide-left'
-        }
-        console.log(isBack);
-        this.$router.isBack = false;
-    }
-};
+    //各种动画方式
+    //methods: Animate.pullUp
+    //methods: Animate.scale
+    //methods: Animate.rotate
+    methods: Animate.custom
+  };
 </script>
 
 <style>
+.page {
+ position: absolute;
+ top:0;
+ left:0;
+}
 </style>
---
+++ src/utils/animate.js
@@ -0,0 +1,65 @@
+/**
+ * 出场／退场 动画
+ *
+ * 可以在Anix函数里修改任意出场动画
+ * https://a-jie.github.io/AniX/
+ */
+
+import ppo from 'ppo.xhtml';
+import { AniX } from 'anix';
+
+export default class Animate {
+
+  //向上拉
+  static pullUp = {
+    enter(el, done) {
+      AniX.fromTo(el, .75,
+        AniX.get({ x: 0, y: 1000, z: 1, normal: { opacity: 1 } }),
+        AniX.get({ x: 0, y: 0, z: 0, normal: { opacity: 1, onComplete: done } }));
+    },
+
+    leave(el, done) {
+      AniX.to(el, .75, AniX.get({ x: 0, y: -1000, normal: { opacity: 0, onComplete: done } }));
+    }
+  }
+
+  //缩放
+  static scale = {
+    enter(el, done) {
+      AniX.fromTo(el, .5,
+        AniX.get({ scale: 0.3, z: 1, normal: { opacity: 0 } }),
+        AniX.get({ scale: 1, z: 0, normal: { opacity: 1, onComplete: done } }));
+    },
+
+    leave(el, done) {
+      AniX.to(el, .5, AniX.get({ scale: 0.3, normal: { opacity: 0, onComplete: done } }));
+    }
+  }
+
+  //选转
+  static rotate = {
+    enter(el, done) {
+      AniX.fromTo(el, .5,
+        AniX.get({ scale: 0.3, y: 500, rotate: 90, z: 1, normal: { opacity: 0 } }),
+        AniX.get({ scale: 1, y: 0, rotate: 0, z: 0, normal: { opacity: 1, onComplete: done } }));
+    },
+
+    leave(el, done) {
+      AniX.to(el, .5, AniX.get({ scale: 0.3, y: -500,rotate: -90, normal: { opacity: 0, onComplete: done } }));
+    }
+  }
+
+  //自定义 随便改
+  static custom = {
+    enter(el, done) {
+      AniX.fromTo(el, .5,
+        AniX.get({ scale: 0.3, y: 500, z: 1, normal: { opacity: 0 } }),
+        AniX.get({ scale: 1, y: 0,  z: 0, normal: { opacity: 1, onComplete: done } }));
+    },
+
+    leave(el, done) {
+      AniX.to(el, .5, AniX.get({ scale: 0.3, y: -500, normal: { opacity: 0, onComplete: done } }));
+    }
+  }
+
+}
